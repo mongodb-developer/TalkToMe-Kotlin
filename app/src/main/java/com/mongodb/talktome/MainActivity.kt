683cb2e61bfd030e6d9a6da888072cebe0cdfea1
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2023-05-05 18:33:22 +0200

    Add ktlint and format files

--- app/src/main/java/com/mongodb/talktome/MainActivity.kt
+++ app/src/main/java/com/mongodb/talktome/MainActivity.kt
@@ -18,7 +18,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             TalkToMeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Greeting("Android")
                 }
             }
